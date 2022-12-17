4ce8dd85f844fc9c0d4d0b396ef8cc8b18b1cc75
zinou-dev <117541891+zinou-dev@users.noreply.github.com>
2022-12-17 18:38:24 +0100

    Add files via upload

---
+++ dad.js
@@ -0,0 +1,13 @@
+window.onload = function (){
+alert("اضغط على صورة الشخصية لاظهار نبذة عنها")
+}
+function myFun() {
+    document.getElementById("demo").style.background= "white";
+    document.getElementsByClassName("myfun").style.display= "none";
+    document.getElementById("dem").style.display="inherit";
+}
+
+function myFunc() {
+    document.getElementById("demo").style.background= "#252525";
+    document.getElementById("dem").style.display= "none";
+    document.getElementById("foot").style.display="inherit";}
